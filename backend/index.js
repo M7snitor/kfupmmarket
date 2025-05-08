602838f01b1aed74e88d8cb736ff3447c43cd1f0
Abdulmohsen Alshawarib <M7snitor@gmail.com>
2025-05-09 00:07:14 +0300

    deploy vercel updates backend

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,13 @@ require('dotenv').config();
 
 const app = express();
 
+// === CORS Fix ===
+app.use(cors({
+  origin: 'https://kfupmmarket-frhv-hl251n84g-m7snitors-projects.vercel.app',
+  credentials: true
+}));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Serve uploaded images
@@ -21,8 +26,8 @@ app.use('/api/messages', require('./routes/message'));
 // Connect DB & Start Server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log('Server running on port', process.env.PORT);
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running on port', process.env.PORT || 5000);
     });
   })
   .catch(err => console.error(err));
